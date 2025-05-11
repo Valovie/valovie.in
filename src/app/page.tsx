7607f87c013cfa5e060f65ab69371380f8ddd023
Valovie <valovie.team@gmail.com>
2025-05-11 19:07:11 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,8 +12,8 @@ import Video from "@/components/Video";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Free Next.js Template for Startup and SaaS",
-  description: "This is Home for Startup Nextjs Template",
+  title: "Valovie",
+  description: "Your Dreams are the mission.Valovie is the arena.",
   // other metadata
 };
 
